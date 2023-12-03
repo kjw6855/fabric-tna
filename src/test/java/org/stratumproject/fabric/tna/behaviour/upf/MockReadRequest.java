007c6c0b95b20900da326cfe276c28853ae65bab
Jiwon Kim <kim1685@purdue.edu>
2023-12-03 11:38:01 +0000

    support ONOS-2.7.1

--- src/test/java/org/stratumproject/fabric/tna/behaviour/upf/MockReadRequest.java
+++ src/test/java/org/stratumproject/fabric/tna/behaviour/upf/MockReadRequest.java
@@ -87,6 +87,16 @@ public class MockReadRequest implements P4RuntimeReadClient.ReadRequest {
     }
 
     @Override
+    public P4RuntimeReadClient.ReadRequest allTableEntries() {
+        return this;
+    }
+
+    @Override
+    public P4RuntimeReadClient.ReadRequest allDefaultTableEntries() {
+        return this;
+    }
+
+    @Override
     public P4RuntimeReadClient.ReadRequest actionProfileGroups(PiActionProfileId actionProfileId) {
         return this;
     }
